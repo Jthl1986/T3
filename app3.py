2b833edab22d960324dd9e4b3c5f044454caa509
Santacecilia <jorgesantacecilia@gmail.com>
2023-04-17 10:40:02 -0300

    9

--- app3.py
+++ app3.py
@@ -423,8 +423,8 @@ def app5():
         gctotal = st.session_state.dfp['Gastos comercialización'].sum()
         mbtotal = st.session_state.dfp['Margen bruto'].sum()
     if df1 is not None:
-        arrend = st.session_state.df1[0]
-        gast = st.session_state.df1[1]
+        arrend = st.session_state.df1[0][0]
+        gast = st.session_state.df1[1][0]
         result = int(mbtotal)-int(arrend)-int(gast)
         # Crear una lista de diccionarios con los datos
         data = [
