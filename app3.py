1fc51b3ed189316591a43c1d8caa51634e4b3356
Santacecilia <jorgesantacecilia@gmail.com>
2023-04-24 21:55:49 -0300

    9

--- app3.py
+++ app3.py
@@ -249,11 +249,10 @@ def app2():
     css()
     right.metric('Los ingresos totales por servicios agrícolas son: ', "${:,}".format(st.session_state["ingresos_totales"]))
     
-    # Botón para borrar la última fila de la tabla
-    if len(st.session_state.dfx) > 0:
-        if st.button("Borrar última fila"):
-            st.session_state.dfx = st.session_state.dfx.iloc[:-1]
-            st.session_state["ingresos_totales"] -= st.session_state.dfx.iloc[-1]["Ingreso estimado"]
+    if st.button("Eliminar último ingreso"):
+        last_row = st.session_state.dfx.tail(1)
+        st.session_state.dfx = st.session_state.dfx.drop(st.session_state.dfx.index[-1])
+        st.session_state["ingresos_totales"] -= last_row["Ingreso estimado"].values[0]
     
     right.write("Tabla para copiar:")
     right.table(st.session_state.dfx.style.format({"Superficie(ha)":"{:.0f}", "Precio":"${:,}", "Ingreso estimado":"${:,}"}))
